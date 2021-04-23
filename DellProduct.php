90008692f2e7112715df510f34b59b6dc9ac6710
brianp702 <79561637+brianp702@users.noreply.github.com>
2021-04-23 12:56:46 -0700

    Remove outdated code

--- DellProduct.php
+++ DellProduct.php
@@ -9,6 +9,7 @@
  * all known data locations would be tried until success. Or, detect the page type and run the 
  * code specialized for that format.
  * For scraping a different web site, I would make a new class for each one. AmazonProduct, WayfairProduct, etc.
+ *
  */
 class DellProduct extends StoreProduct 
 {
